f00b4896b7b421179a4d7f774d697a03aae896c7
RobbyPat <logvinenko.e.s@gmail.com>
2016-09-16 01:15:53 +0300

    third commit

---
+++ login.php
@@ -0,0 +1,69 @@
+<link rel="stylesheet" type="text/css" href="./css/style.css">
+
+<?php
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db = "tasks";
+
+mysql_connect($host, $user, $pass);
+mysql_select_db($db);
+
+$mail=$pass="";
+
+if (isset($_POST['email']))
+{
+    $mail=sanitize_string($_POST['email']);
+    $pass=sanitize_string($_POST['password']);
+    if ($mail=='' || $pass=='') echo $error="</br></br> <label class='error'>You entered data is not in all fields</label>";
+    else
+    {
+        $temppass=md5($pass);
+        $query="SELECT * FROM users WHERE email='$mail' AND pass='".md5($pass)."'";
+        if (mysql_num_rows(query_mysql($query)))
+        {
+            $_SESSION['email']=$mail;
+            $_SESSION['pass']=md5($pass);
+            die("</br><br/><label class='error'>Login success</label></br><a href='title.html' id='a'>Go to create TODO List</a>");
+          
+        }
+        else echo $error="</br> <label class='error'>Invalid login or password...</label>";
+    }    
+}
+
+echo <<< _FORMLOGIN
+
+<form method="post" id="log" action='login.php'>
+<br/></br>
+<h3>Login to your personal page</h3></br>
+<label>Enter your email and password</label></br></br>
+<input type="email" name="email" id="email" placeholder="Email"></br></br>
+<input type="password" name="password" id="password" placeholder="Password"></br></br>
+<button type="submit" value="submit" id="submit">Submit</button>
+</form>
+
+_FORMLOGIN;
+
+function query_mysql($query)
+{
+    $result=mysql_query($query) or die("Unable to query the database: ".mysql_error());
+    return $result;
+}
+
+function destroy_session()
+{
+    $_SESSION=array();
+    if (session_id()!="" || isset($_COOKIE[session_name()]))
+    setcookie(session_name(), '', time()-2592000,'/');
+    session_destroy();
+}
+
+function sanitize_string($var)
+{
+        $var=strip_tags($var);
+        $var=stripcslashes($var);
+        $var=htmlentities($var, ENT_QUOTES, 'cp1251');
+        return mysql_real_escape_string($var);
+ }
+
+?>
---
+++ reg.php
@@ -0,0 +1,76 @@
+<link rel="stylesheet" type="text/css" href="./css/style.css">
+
+<?php
+
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db = "tasks";
+
+mysql_connect($host, $user, $pass);
+mysql_select_db($db);
+
+$error=$pass=$mail=$pass2="";
+
+if (isset($_SESSION['email'])) destroy_session();
+
+if (isset($_POST['email']))
+{
+    $mail=$_POST['email'];
+    $pass=$_POST['password'];
+    $pass2=$_POST['password2'];
+    if ($mail=='' || $pass=='' || $pass2=='') echo $error="<br/><br/><label class='error'>Error. Please enter in all the filds...</label><br /><br />";
+    else
+    {
+        $query="SELECT * FROM users WHERE email='$mail'";
+        if (mysql_num_rows(query_mysql($query)))
+        {
+            echo $error="<br/><br/><label class='error'>This user is already exists. Please choose another email.</label><br /><br />";
+        }
+        else
+        {
+            if ($pass!=$pass2)
+            {
+                echo $error="<br/><br/><label class='error'>Passwords do not match...</label><br /><br />";
+            }
+            else
+            {
+                $temppass=md5($pass);
+                $query="INSERT INTO users VALUES('$mail', '$temppass')";
+                query_mysql($query);
+                die ("<br/><h4>Your account has been created</h4><br/><label class='error'>Please sign in using your login and password...</label><br/><a href='login.php' id='a'>Login</a>");
+            }
+        }
+    }
+}
+
+echo <<< _REGFORM
+
+<form method="post" action="reg.php" id="reg">
+<br/>
+<h3>Registration form</h3><br/>
+<label>Enter your email and password</label><br/><br/>
+<input type="email" name="email" id="email" placeholder="Email"><br/><br/>
+<input type="password" name="password" id="password" placeholder="Password"><br/><br/>
+<input type="password" name="password2" id="password2" placeholder="Repeat the password"><br/><br/>
+<button type="submit" value="submit" id="submit">Submit</button>
+</form>
+
+_REGFORM;
+
+function query_mysql($query)
+{
+    $result=mysql_query($query) or die("Unable to query the database: ".mysql_error());
+    return $result;
+}
+
+function destroy_session()
+{
+    $_SESSION=array();
+    if (session_id()!="" || isset($_COOKIE[session_name()]))
+    setcookie(session_name(), '', time()-2592000,'/');
+    session_destroy();
+}
+
+
+?>
